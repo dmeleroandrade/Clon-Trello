f7d91f7ca4e3f2739e399cf7b3902f222c1ea025
David <dmeleroandrade@gmail.com>
2024-03-15 13:43:59 +0100

    "importado getters"

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,9 @@ import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
 import state from './state'
 import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
+
 
 Vue.use(Vuex)
 
@@ -12,7 +15,7 @@ export default new Vuex.Store({
     strict: debug,
     plgins: debug ? [createLogger()] : [],
     state,
-    getters: {},
-    actions: {},
+    getters,
+    actions,
     mutations
 })
